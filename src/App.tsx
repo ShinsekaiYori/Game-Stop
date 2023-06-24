cd266bba9560ab1e4cea696b32722b723f82a6ad
“Shubham <yadavshubham4520@gmail.com>
2023-06-24 21:15:13 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" `,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
+
+// 1. templeareas - to define the layout of our grid.
+//2.  To ensure our side panel is shown only on large devices
+// , we wrap it inside a show  component.
+//3. attribute like above and below.Above means lg and above.
