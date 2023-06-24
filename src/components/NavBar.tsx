d0402269395d50b61c8b029fbe127a05858fc62e
“Shubham <yadavshubham4520@gmail.com>
2023-06-24 22:07:22 +0530

    Build a NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
+
+//1. HStack is for horizontal placement.
+//2.Import the image like a regular javscript component
