061556ca86f3cdc57bf0abcf10685b4f8e8836d2
“Shubham <yadavshubham4520@gmail.com>
2023-06-26 21:39:41 +0530

    Display Gneres.

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,10 +7,15 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="270px" borderRadius={10} overflow="hidden">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
 };
 
 export default GameCardContainer;
+
+//1.We removed fixed card size because we did not assign size
+// to the size pnale and fixed it.Now, with fixed width
+//the grid takes all the available face.So , the space gets equally distributed
+// between the 3 containers.
