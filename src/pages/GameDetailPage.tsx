6e6715b1bafa5835d04e9051ef67d943fdc3fb61
“Shubham <yadavshubham4520@gmail.com>
2023-07-07 22:53:09 +0530

    Built game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -7,6 +7,7 @@ import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -22,6 +23,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
