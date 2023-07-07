6278caef510b90ef02f918e5de75bcd64f0f5878
“Shubham <yadavshubham4520@gmail.com>
2023-07-07 23:41:02 +0530

    Corrected errors

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,6 +1,6 @@
 import { Heading } from "@chakra-ui/react";
 import usePlatform from "../hooks/usePlatform";
-import useGenre from "../hooks/useGenres";
+import useGenre from "../hooks/useGenre";
 import useGameQueryStore from "../store";
 
 const GameHeading = () => {
